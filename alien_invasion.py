7876b786cabe99456c65c281c1d1dc810fb30c92
sloanalycia <39379457+sloanalycia@users.noreply.github.com>
2019-01-23 14:00:40 -0600

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,25 +12,29 @@ import game_functions as gf
 def run_game():
 	
 	#Initialize game and create a screen object
-	pygame.init()
-	ai_settings = Settings()
+	
+	pygame.init() # this initializes background settings for pygame to work properly
+	ai_settings = Settings()# This puts the Settings class into ai_settings variable. You can call this then and access settings
 	screen = pygame.display.set_mode(
-	(ai_settings.screen_width, ai_settings.screen_height))
-	pygame.display.set_caption("Alien Invasion")
+	(ai_settings.screen_width, ai_settings.screen_height))#display is a module and set_mode is a function that initializes pygames Surface class.
+	#It then gives it the variable name 'screen'
+	#It also calls the ai_settings from settings import of Settings, called above, and calls the height and width
+	pygame.display.set_caption("Alien Invasion")# this sets the windows caption title, it's visible when you run the game
 	
 	#make a ship
-	ship = Ship(ai_settings,screen)
+	ship = Ship(ai_settings,screen) #ship class has been imported above, this calls the ships settings and to the screen
 	#make a group to store bullets in
-	bullets = Group() 
+	bullets = Group() # here we make an instance of Group and call it bullets
 	
 	
 	#Set the background color
 	bg_color = (230,230,230)
 	
 	#Start the main loop for the game
+	#I believe this runs through the functions in other files and runs them thru for the game
 	while True:
 		gf.check_events(ai_settings, screen, ship, bullets)
-		ship.update()
+		ship.update()#updates the ship as necessary
 		gf.update_bullets(bullets)
 		gf.update_screen(ai_settings,screen,ship,bullets)
 		
@@ -39,4 +43,4 @@ def run_game():
 		
 				
 			
-run_game()
+run_game()# this runs the whole thing
