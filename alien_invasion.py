f62834f94ddb3ac21bec6e3d3add0cb05104ef80
sloanalycia <39379457+sloanalycia@users.noreply.github.com>
2019-01-24 13:07:51 -0600

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,7 +25,7 @@ def run_game():
 	aliens = Group()
 	
 	#create a fleet of aliens
-	gf.create_fleet(ai_settings, screen,aliens):
+	gf.create_fleet(ai_settings, screen,ship, aliens):
 	
 	#Set the background color
 	bg_color = (230,230,230)
