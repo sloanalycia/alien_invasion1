154c39f77a6da804c5dd775f3f5489b38585e6d9
sloanalycia <39379457+sloanalycia@users.noreply.github.com>
2019-01-25 11:30:16 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings():
 		self.ship_speed_factor = 1.5
 		
 		#bullet settings
-		self.bullet_speed_factor = 1
+		self.bullet_speed_factor = 3
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = 80,60,60
