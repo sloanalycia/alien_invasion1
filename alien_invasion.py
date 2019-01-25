4c534bddd60d5b3523be834fcac34881d78f2906
sloanalycia <39379457+sloanalycia@users.noreply.github.com>
2019-01-25 14:25:41 -0600

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from pygame.sprite import Group
 
 from alien import Alien
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 import game_functions as gf
 
@@ -18,6 +19,7 @@ def run_game():
 	screen = pygame.display.set_mode(
 	(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
+	stats = GameStats(ai_settings)
 	
 	#make a ship*
 	ship = Ship(ai_settings,screen)
@@ -40,7 +42,7 @@ def run_game():
 		ship.update()
 		gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
 		gf.update_aliens(ai_settings,ship, aliens)
-		gf.update_screen(ai_settings,screen,ship,aliens,bullets)
+		gf.update_screen(ai_settings,stats,screen,ship,aliens,bullets)
 		
 		
 		
