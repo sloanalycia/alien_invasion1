f9d6fcde00d647f0d1b365d7c265e2f2a060e78d
sloanalycia <39379457+sloanalycia@users.noreply.github.com>
2019-01-25 15:02:20 -0600

    Update game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -4,6 +4,8 @@ class GameStats():
         def __init__(self, ai_settings):
                 self.ai_settings = ai_settings
                 self.reset_stats()
+                #start alien invasion in active state
+                self.game_active = True
                 
          def reset_stats(self):
                 self.ships_left = self.ai_settings.ships_limit
